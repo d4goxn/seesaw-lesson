e8c9766e40d94ec52d565476cce4ea62782522b2
Dan Ross <ross9885@gmail.com>
2015-11-20 14:49:25 +0100

    Add a button, make it do stuff

--- src/seesaw_demo/core.clj
+++ src/seesaw_demo/core.clj
@@ -2,9 +2,23 @@
   (:use seesaw.core)
   (:gen-class))
 
+(native!)
+
+(def b (button :text "shh!"))
+
+(def ls (vertical-panel
+          :items ["Ima window!" "I'm with stupid ^"]))
+
+(def f (frame :title "Haro"
+              :content (vertical-panel
+                         :items [ls b])
+              :on-close :exit))
+
+(listen b :action (fn [e]
+                    (alert e "ok")
+                    (config! ls :items ["..."])
+                    (config! b :enabled? false)
+                    (pack! f)))
+
 (defn -main [& args]
-  (invoke-later (-> (frame :title "Haro"
-                           :content "Ima window!"
-                           :on-close :exit)
-                    pack!
-                    show!)))
+  (invoke-later (-> f pack! show!)))
